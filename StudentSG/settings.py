746b8ce7e386f262f528e4e2383ac41599e89173
Alexey <aleksejdelov@gmail.com>
2021-04-16 10:55:41 +0300

    Добавлена база данных

--- StudentSG/settings.py
+++ StudentSG/settings.py
@@ -77,11 +77,9 @@ WSGI_APPLICATION = 'StudentSG.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
